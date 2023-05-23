d946b8ba060fdbd43c2282d3e738bf73b41a4ea6
Damilola Odumosu <dt.odumosu@gmail.com>
2023-05-23 17:04:22 +0100

    javascript completed

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+const divContainer = document.getElementById("tvEpisode");
+
+episodeList.forEach(episode => {
+  const episodeDiv = document.createElement("div");
+  episodeDiv.classList.add("episodeCard");
+
+  episodeDiv.innerHTML = `
+  <h2>${episode.name} 
+  S${episode.season.toString().padStart(2, "0")}E${episode.number
+    .toString()
+    .padStart(2, "0")} </h2>
+  
+    <img src = "${episode.image.medium}">
+    <span>${episode.summary}</span>
+  `;
+
+
+
+
+divContainer.appendChild(episodeDiv)
+});
+
+
+
+
+
+
+
+
+
+
 }
 
 window.onload = setup;
