f39edda8c3cbc6a50fe41344bdf9a57b94707f36
Damilola Odumosu <dt.odumosu@gmail.com>
2023-05-30 09:34:57 +0100

    completed level 300

--- script.js
+++ script.js
@@ -1,54 +1,77 @@
-//You can edit ALL of the code here
-  function setup() {
-    const allEpisodes = getAllEpisodes();
-    makePageForEpisodes(allEpisodes);
-    searchEpisode();
-    
-  } 
+const rootElem = document.getElementById("root");
+const divContainer = document.getElementById("tvEpisode");
 
-  function makePageForEpisodes(episodeList) {
-    const rootElem = document.getElementById("root");
-    rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+  searchEpisode(allEpisodes);
+  selectEpisode(allEpisodes);
+}
 
-    const divContainer = document.getElementById("tvEpisode");
+function makePageForEpisodes(episodeList) {
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    const episodeDiv = document.createElement("div");
+    episodeDiv.classList.add("episodeCard");
+    episodeDiv.innerHTML = `
+      <h2>${episode.name}
+      S${episode.season.toString().padStart(2, "0")}E${episode.number
+      .toString()
+      .padStart(2, "0")} </h2>
+        <img src = "${episode.image.medium}">
+        <span>${episode.summary}</span>
+      `;
+    divContainer.appendChild(episodeDiv);
+  });
+}
 
-    episodeList.forEach((episode) => {
-      const episodeDiv = document.createElement("div");
-      episodeDiv.classList.add("episodeCard");
-
-      episodeDiv.innerHTML = `
-    <h2>${episode.name} 
-    S${episode.season.toString().padStart(2, "0")}E${episode.number
-        .toString()
-        .padStart(2, "0")} </h2>
-    
-      <img src = "${episode.image.medium}">
-      <span>${episode.summary}</span>
-    `;
-
-      divContainer.appendChild(episodeDiv);
-    });
-  }
-
-  // Adding Search level 200
-
-function searchEpisode(episodes){
-
+//   // Adding Search level 200
+function searchEpisode(episodes) {
   let searchInput = document.getElementById("searchInput");
   searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase();
-      rootElem.textContent = "";
-
-
+    divContainer.textContent = "";
     let searchedEpisode = episodes.filter((episode) => {
       return (
         episode.name.toLowerCase().includes(value) ||
         episode.summary.toLowerCase().includes(value)
       );
-      // const searchCard = document.getElementsByClassName("episodeCard");
-      // searchCard.classList.toggle("hide", !isVisible);
-makePageForEpisodes(searchedEpisode);
     });
-  }); 
-} 
-  window.onload = setup;
+    makePageForEpisodes(searchedEpisode);
+  });
+}
+// Adding level 300
+
+function selectEpisode(episodes) {
+  const select = document.getElementById("select");
+  const defaultOption = document.createElement("option");
+  defaultOption.innerText = "Select an episode";
+  select.appendChild(defaultOption);
+
+  episodes.forEach((episode) => {
+    const optionForEp = document.createElement("option");
+    optionForEp.value = episode.name;
+    optionForEp.innerText = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+      episode.name
+    }`;
+    select.appendChild(optionForEp);
+  });
+
+  select.addEventListener("change", (e) => {
+    let value = e.target.value;
+    divContainer.textContent = "";
+    let filteredEpisode = episodes.filter((episode) => {
+      if (episode.name.includes(value) || episode.summary.includes(value)) {
+        return episode;
+      } else if (value === defaultOption.innerText) {
+        return episodes;
+      }
+    });
+    makePageForEpisodes(filteredEpisode);
+  });
+}
+// Adding level 350
+
+window.onload = setup;
